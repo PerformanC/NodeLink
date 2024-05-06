baf30ba302191258f06e1f5167ea0e3e8966e7bc
ThePedroo <pedroolimpioguerra@gmail.com>
2024-05-06 01:29:38 -0300

    fix: functions position
    This commit fies the position of the functions in BandCamp source file.

--- src/sources/bandcamp.js
+++ src/sources/bandcamp.js
@@ -1,6 +1,79 @@
 import config from '../../config.js'
 import { debugLog, makeRequest, encodeTrack } from '../utils.js'
 
+async function search(query, shouldLog) {
+  if (shouldLog) debugLog('search', 4, { type: 1, sourceName: 'BandCamp', query })
+
+  const { body: data } = await makeRequest(`https://bandcamp.com/search?q=${encodeURI(query)}&item_type=t&from=results`, { method: 'GET' })
+
+  let names = data.match(/<div class="heading">\s+<a.*?>(.*?)<\/a>/gs)
+
+  if (!names) {
+    if (shouldLog) debugLog('search', 4, { type: 3, sourceName: 'BandCamp', query, message: 'No matches found.' })
+
+    return {
+      loadType: 'empty',
+      data: {}
+    }
+  }
+
+  const tracks = []
+
+  if (names.length > config.options.maxSearchResults)
+    names = names.slice(0, config.options.maxSearchResults)
+  
+  names.forEach((name) => {
+    tracks.push({
+      encoded: null,
+      info: {
+        identifier: null,
+        isSeekable: true,
+        author: null,
+        length: -1,
+        isStream: false,
+        position: 0,
+        title: name[1].trim(),
+        uri: null,
+        artworkUrl: null,
+        isrc: null,
+        sourceName: 'bandcamp'
+      },
+      pluginInfo: {}
+    })
+  })
+
+  const authors = data.match(/<div class="subhead">\s+(?:from\s+)?[\s\S]*?by (.*?)\s+<\/div>/gs)
+
+  authors.forEach((author, i) => {
+    tracks[i].info.author = author.split('by')[1].split('</div>')[0].trim()
+  })
+
+  const artworkUrls = data.match(/<div class="art">\s*<img src="(.+?)"/gs)
+
+  artworkUrls.forEach((artworkUrl, i) => {
+    tracks[i].info.artworkUrl = artworkUrl.split('"')[3].split('"')[0]
+  })
+
+  const urls = data.match(/<div class="itemurl">\s+<a.*?>(.*?)<\/a>/gs)
+
+  urls.forEach((url, i) => {
+    tracks[i].info.uri = url.split('">')[2].split('</a>')[0]
+    
+    const identifier = tracks[i].info.uri.match(/^https?:\/\/([^/]+)\/track\/([^/?]+)/)
+    tracks[i].info.identifier = `${identifier[1]}:${identifier[2]}`
+
+    tracks[i].encoded = encodeTrack(tracks[i].info)
+    tracks[i].pluginInfo = {}
+  })
+
+  if (shouldLog) debugLog('search', 4, { type: 2, sourceName: 'BandCamp', tracksLen: tracks.length, query })
+
+  return {
+    loadType: 'search',
+    data: tracks
+  }
+}
+
 async function loadFrom(url) {
   const { body: data } = await makeRequest(url, { method: 'GET' })
   const matches = /<script type="application\/ld\+json">([\s\S]*?)<\/script>/.exec(data)
@@ -90,79 +163,6 @@ async function loadFrom(url) {
   }
 }
 
-async function search(query, shouldLog) {
-  if (shouldLog) debugLog('search', 4, { type: 1, sourceName: 'BandCamp', query })
-
-  const { body: data } = await makeRequest(`https://bandcamp.com/search?q=${encodeURI(query)}&item_type=t&from=results`, { method: 'GET' })
-
-  let names = data.match(/<div class="heading">\s+<a.*?>(.*?)<\/a>/gs)
-
-  if (!names) {
-    if (shouldLog) debugLog('search', 4, { type: 3, sourceName: 'BandCamp', query, message: 'No matches found.' })
-
-    return {
-      loadType: 'empty',
-      data: {}
-    }
-  }
-
-  const tracks = []
-
-  if (names.length > config.options.maxSearchResults)
-    names = names.slice(0, config.options.maxSearchResults)
-  
-  names.forEach((name) => {
-    tracks.push({
-      encoded: null,
-      info: {
-        identifier: null,
-        isSeekable: true,
-        author: null,
-        length: -1,
-        isStream: false,
-        position: 0,
-        title: name[1].trim(),
-        uri: null,
-        artworkUrl: null,
-        isrc: null,
-        sourceName: 'bandcamp'
-      },
-      pluginInfo: {}
-    })
-  })
-
-  const authors = data.match(/<div class="subhead">\s+(?:from\s+)?[\s\S]*?by (.*?)\s+<\/div>/gs)
-
-  authors.forEach((author, i) => {
-    tracks[i].info.author = author.split('by')[1].split('</div>')[0].trim()
-  })
-
-  const artworkUrls = data.match(/<div class="art">\s*<img src="(.+?)"/gs)
-
-  artworkUrls.forEach((artworkUrl, i) => {
-    tracks[i].info.artworkUrl = artworkUrl.split('"')[3].split('"')[0]
-  })
-
-  const urls = data.match(/<div class="itemurl">\s+<a.*?>(.*?)<\/a>/gs)
-
-  urls.forEach((url, i) => {
-    tracks[i].info.uri = url.split('">')[2].split('</a>')[0]
-    
-    const identifier = tracks[i].info.uri.match(/^https?:\/\/([^/]+)\/track\/([^/?]+)/)
-    tracks[i].info.identifier = `${identifier[1]}:${identifier[2]}`
-
-    tracks[i].encoded = encodeTrack(tracks[i].info)
-    tracks[i].pluginInfo = {}
-  })
-
-  if (shouldLog) debugLog('search', 4, { type: 2, sourceName: 'BandCamp', tracksLen: tracks.length, query })
-
-  return {
-    loadType: 'search',
-    data: tracks
-  }
-}
-
 async function retrieveStream(uri, title) {
   const { body: data } = await makeRequest(uri, { method: 'GET' })
 
@@ -188,7 +188,7 @@ async function retrieveStream(uri, title) {
 }
 
 export default {
-  loadFrom,
   search,
+  loadFrom,
   retrieveStream
 }
