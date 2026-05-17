2b757091f64d0800fbf4c05fd920fed373522b2f
Ramkrishna <144999854+ramsquishna@users.noreply.github.com>
2026-05-17 09:20:28 +0530

    fix(spellings): dynamicly to dynamically
    Signed-off-by: Ramkrishna <144999854+ramsquishna@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -2702,7 +2702,7 @@ process.on('unhandledRejection', (reason, promise) => {
 
 if (clusterEnabled && cluster.isPrimary) {
   if (config.sources?.youtube?.getOAuthToken) {
-    // dynamicly import OAuth (if enabled)
+    // dynamically import OAuth (if enabled)
     const OAuth = (
       await import('./sources/youtube/OAuth.ts').catch((e) => {
         logger(
