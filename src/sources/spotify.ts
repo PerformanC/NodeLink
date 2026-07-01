8cd02de4faaa6a730513674d73559eba182cfd03
rafael <rafaeldasilvadeveloper@gmail.com>
2026-07-01 02:35:04 -0300

    fix(spotify): use search.maxResults configuration for search limit

--- src/sources/spotify.ts
+++ src/sources/spotify.ts
@@ -777,14 +777,7 @@ export default class SpotifySource implements SourceInstance {
       return this.getRecommendations(query)
 
     try {
-      let limit = this.config.playlistLoadLimit || 10
-
-      // fixes the error Argument <limit> for field /searchV2 cannot be greater than 1000
-      // this is a config issue from the user side, but if this can be used as a workaround,
-      // we set it to 10 to avoid the error :p.
-      if (limit > 999) {
-        limit = 10
-      }
+      let limit = this.nodelink.options.search.maxResults || 10
 
       // Priority 1: Internal Search (Rich nodes + Local matching)
       if (this.anonymousToken || this.config.sp_dc) {
